16cba3463a3ab67e78874d8a3845c58e2557c62d
wissembhk2 <158522424+wissembhk2@users.noreply.github.com>
2024-02-24 11:03:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var el= document.getElementById('test').children[4];
-var totalprice= document.getElementById('totalprice');
-var btn= document.getElementById('btn');
-
-btn.addEventListener('click',function(){
-    totalprice.innerHTML=parseInt(totalprice.innerHTML)+parseInt(el.innerHTML)
-})
+var el= document.getElementsByClassName('btn');
+var totalprice = document.getElementById('totalprice');
+for ( let e of el)
+{
+    e.addEventListener('click',function(){
+       totalprice.innerHTML=parseInt(e.parentElement.children[4].innerHTML) + parseInt(totalprice.innerHTML)
+    })
+}
