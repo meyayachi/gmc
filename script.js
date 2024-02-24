bda262189bf5ffe7557fa88a48b79fc4ccca8edb
meyayachi <153979342+meyayachi@users.noreply.github.com>
2024-02-24 11:18:45 +0100

    Update script.jsmey

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 var el= document.getElementsByClassName('btn');
 var totalprice = document.getElementById('totalprice');
-for ( let e of el)
+var removeButtons = document.getElementsByClassName('btn');
+var totalprice = document.getElementById('totalprice');
+case 1: ( let e of el)
 {
     e.addEventListener('click',function(){
        totalprice.innerHTML=parseInt(e.parentElement.children[4].innerHTML) + parseInt(totalprice.innerHTML)
     })
 }
+case 2: (let btn of removeButtons) {
+    btn.addEventListener('click', function() {
+        var itemPrice = parseInt(btn.parentElement.children[5].innerHTML);
+        var currentTotal = parseInt(totalprice.innerHTML);
+        totalprice.innerHTML = currentTotal - itemPrice; 
+    }
+
